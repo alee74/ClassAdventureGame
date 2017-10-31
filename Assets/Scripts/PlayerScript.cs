a89a6ecc0897d3765050bb9024f8842d345c04a8
sahough <shough@trinity.edu>
2017-10-31 15:16:43 -0500

    Added left-right movement

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,16 +13,16 @@ enum State
 public class PlayerScript : MonoBehaviour {
 
     Rigidbody2D rgb;
-   // Animator anim;
+    Animator anim;
     State state;
-    float speed = 10f;
+    float speed = 5f;
     bool isFacingRight;
 
 
 	// Use this for initialization
 	void Start () {
         rgb = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         state = State.Stand;
 	}
 	
@@ -52,20 +52,20 @@ public class PlayerScript : MonoBehaviour {
     void ChangeState(State newState)
     {
         state = newState;
-        Debug.Log(state);
-        /*       switch (state)
+
+               switch (state)
                {
                    case State.Stand:
-                       //anim.SetInteger("action", 0);
-
+                       anim.SetInteger("action", 0);
                        break;
                    case State.Walk:
+					   anim.SetInteger("action", 1);
                        break;
                    case State.Jump:
                        break;
                    case State.Punch:
                        break;
-               }*/
+               }
     }
 
     void DirectionFacing()
@@ -73,17 +73,29 @@ public class PlayerScript : MonoBehaviour {
         float inX = Input.GetAxis("Horizontal");
         if (inX > 0)
         {
+			//anim.SetBool("isFacingRight", true);
+			Debug.Log("right");
             isFacingRight = true;
         }
         else if (inX < 0)
         {
+			//anim.SetBool("isFacingRight", false);
+			Debug.Log("left");
             isFacingRight = false;
         }
+
+		if(isFacingRight) {
+			rgb.transform.localScale = new Vector3 (Mathf.Abs(rgb.transform.localScale.x), rgb.transform.localScale.y, rgb.transform.localScale.z);
+		}
+		else {
+			rgb.transform.localScale = new Vector3 (-Mathf.Abs(rgb.transform.localScale.x), rgb.transform.localScale.y, rgb.transform.localScale.z);
+		}
     }
  
 
     void Stand()
     {
+		//rgb.velocity = new Vector2 (0, 0);
         float inX = Input.GetAxis("Horizontal");
         bool inY = Input.GetKeyDown("space");
         if (inX != 0)
@@ -96,14 +108,14 @@ public class PlayerScript : MonoBehaviour {
     {
         float inX = Input.GetAxis("Horizontal");
         bool inY = Input.GetKeyDown("space");
-        if (inX == 0)
-        {
-            ChangeState(State.Stand);
-        }
-        else
-        {
-            rgb.AddForce(new Vector2(speed * inX, 0));
-        }
+		if (inX == 0) {
+			ChangeState (State.Stand);
+
+		} else if (inX > 0) {
+			rgb.velocity = new Vector2 (inX*speed, 0);
+		} else if (inX < 0) {
+			rgb.velocity = new Vector2 (inX*speed, 0);
+		}
 
     }
          /*   if (inY)
