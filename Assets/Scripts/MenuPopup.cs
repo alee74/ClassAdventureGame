320fffe5869270bc3aa7d0c6ddc1289dbfd8b947
Lauren Truong <lauren@Laurens-MBP.lan>
2017-10-31 17:12:39 -0500

    menuPopup isn't working. Pop up occurs when i is pressed but when pressed again does not disappear

---
+++ Assets/Scripts/MenuPopup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class MenuPopup : MonoBehaviour {
+
+	public GameObject menuPopup;
+	bool menuIsUp;
+
+	// Use this for initialization
+	void Start () {
+		menuPopup.SetActive (false); 
+		menuIsUp = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Inventory")) {
+			if (menuIsUp == false) {
+				menuPopup.SetActive (true); 
+				menuIsUp = true; 
+			}
+		}
+		if (Input.GetButtonDown("Inventory")){
+			if (menuIsUp == true) {
+				menuPopup.SetActive (false); 
+				menuIsUp = false; 
+			}
+		}
+	}
+}
