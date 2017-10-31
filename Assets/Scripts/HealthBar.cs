90807ab50166cf19966bbe48c259946d796e3059
Jada Codwell <jcodwell@trinity.edu>
2017-10-31 12:59:43 -0500

    Added Hospital and Health Bar UI
    Hospital Code - Restores health when character walks by or enters
    
    Health Bar - Basic code to implement a UI bar using images

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	[SerializeField]
+	private float fillAmnt;
+
+	[SerializeField]
+	private Image fillcontent; // Image Used to Fill bar
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		FillBar ();  //Updates the bar to fill the image	
+	
+	}
+
+	private void FillBar() {
+	
+		fillcontent.fillAmount = CalculatedStats(100, 0, 100, 0, 1);
+	}
+
+	// value - Takes current Health 
+	// max - Is the max health
+	// min = 0
+	// retMin = o 
+	// retMax = 1 
+
+
+	private float CalculatedStats(float value, float min, float max, float retMax, float retMin) {  
+													
+
+
+		return (value - min) * (retMax - retMin) / (max - min) + retMin;
+	
+	}
+
+}
+
---
+++ Assets/Scripts/Hospital.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Hospital : MonoBehaviour {
+
+
+	private int health; 
+	private int maxhealth = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		// Resource menu Buy or upgrade health Items
+
+
+		// Inventory
+
+
+
+		// Restore Player Health
+	
+
+
+
+
+	}
+
+	//add method to view inventory
+
+	public void OnTriggerEnter2D(Collider2D Player) {
+	
+		if (health <= 100) {
+			health += maxhealth - health;
+		
+		} else {
+			
+			Debug.Log ("Your Health Is Full");
+		
+		}
+			
+
+
+	
+	}
+
+}
