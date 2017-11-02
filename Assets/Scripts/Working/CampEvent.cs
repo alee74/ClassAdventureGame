b03e48aeafd4223c63237f771e9da3daeff57403
KaylaHood <kaylahood1996@gmail.com>
2017-11-02 16:43:21 -0500

    Fixed stuff.

---
+++ Assets/Scripts/Working/CampEvent.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class CampEvent
+{
+    public string name;
+    public string message;
+    public int food;
+    public int water;
+    public int wood;
+}
