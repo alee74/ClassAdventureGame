3f7973681677ad6152973a667e54431f820d5f22
KaylaHood <kaylahood1996@gmail.com>
2017-11-02 15:19:53 -0500

    Added Script Files for characters and events

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Character
+{
+    private string name;
+    private GameObject prefab;
+    private int health;
+    private int stamina;
+    private int strength;
+    private int max_health;
+    private int max_stamina;
+    private int max_strength;
+
+}
---
+++ Assets/Scripts/EventSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Weather { Sunny, Rainy, Cloudy, Stormy };
+
+static public class EventSystem {
+
+    static public List<Character> GetEncounterableCharacters(int day)
+    {
+        float rand = UnityEngine.Random.value;
+
+    }
+
+    static public Weather GetWeather(int day)
+    {
+        int weather = (int)(UnityEngine.Random.value * (Enum.GetNames(typeof(Weather)).Length));
+        return (Weather)weather;
+    }
+
+    static public List<CampEvent> GetCampEvents(int day)
+    {
+        float rand = UnityEngine.Random.value;
+
+    }
+	
+}
