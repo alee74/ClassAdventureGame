505995fb0d055fab7e5b7c9ac3930202f1333001
daniellekao <dkao@trinity.edu>
2017-11-02 20:57:34 -0500

    working on item options popUp, bare bones functionality

---
+++ Assets/Scripts/ItemOptions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemOptions : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void OnClick()
+    {
+        gameObject.SetActive(true);
+        transform.position = Input.mousePosition;
+
+    }
+
+    public void OnClose()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PopulatePopUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulatePopUp : MonoBehaviour {
+
+    public Image popUp;
+    private Text popUpTxt;
+
+	// Use this for initialization
+	void Start () {
+        popUpTxt = popUp.GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FillPopUpWithItemOptions()
+    {
+        // here we can check what was selected based on tags......!
+        popUpTxt.text = gameObject.tag + " was selected.";
+    }
+}
