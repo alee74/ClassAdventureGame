0880aa0c37a049c1a6e364bfe954f36ebb8503a8
szbidi <szbidi@trinity.edu>
2017-11-04 18:04:44 -0500

    working on dropping items

--- Assets/Scripts/DropItems.cs
+++ Assets/Scripts/DropItems.cs
@@ -7,22 +7,28 @@ public class DropItems : MonoBehaviour {
 	private ItemsInInventory itemsInInventoryScript;
 	// Use this for initialization
 	void Start () {
-	//	itemsInInventoryScript = GameObject.Find("Inventory").GetComponent<ItemsInInventory>();
-
+		itemsInInventoryScript = /*GameObject.Find("Main Camera").*/GetComponent<ItemsInInventory>();
+		//itemsInInventoryScript
+		Debug.Log(GameObject.Find("Player").gameObject.transform.position.x);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.F)) {
-			Instantiate (resource, new Vector3 (transform.position.x+1, transform.position.y, transform.position.z), Quaternion.identity);
-			if (resource.gameObject.tag == "Food") {
-			//	itemsInInventoryScript.food -= 1;
+		
+			
+	}
+
+	public void dropResource(GameObject resource){
+		Instantiate (resource, new Vector3 (transform.position.x+1, transform.position.y, transform.position.z), Quaternion.identity);
+		if (resource.gameObject.tag == "Food") {
+			itemsInInventoryScript.num_food -= 1;
+			Debug.Log (GameObject.Find ("food(Clone)").gameObject.transform.position);
 			//	Debug.Log (itemsInInventoryScript.food);
-			} else if (resource.gameObject.tag == "Wood") {
-			//	itemsInInventoryScript.wood -= 1;
-			} else if (resource.gameObject.tag == "Water") {
-			//	itemsInInventoryScript.water -= 1;
-			}
+		} else if (resource.gameObject.tag == "Wood") {
+			itemsInInventoryScript.num_wood -= 1;
+		} else if (resource.gameObject.tag == "Water") {
+			itemsInInventoryScript.num_water -= 1;
 		}
 	}
+
 }
