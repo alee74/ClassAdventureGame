aacd6bc80ce7cb4f5ec07eade00a25fdd5776678
KaylaHood <kaylahood1996@gmail.com>
2017-11-07 15:24:55 -0600

    Doing work on Camp Events

--- Assets/Scripts/CampEvent.cs
+++ Assets/Scripts/CampEvent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using System.Xml.Linq;
 
 public class CampEvent
 {
@@ -11,4 +12,24 @@ public class CampEvent
     public int food;
     public int water;
     public int wood;
+}
+
+public static class CampEventSystem
+{
+    public static XElement campEventXML = XElement.Load(@"CampEvents.xml");
+
+    public static CampEvent GetRandomCampEvent(float rand)
+    {
+        int eventIdx = (int)(rand * GetNumberOfEvents());
+        IEnumerable<XElement> events = campEventXML.DescendantsAndSelf("Event");
+        XElement xmlEvent = events.ElementAt(eventIdx);
+        CampEvent campEvent = new CampEvent();
+
+    }
+
+    public static int GetNumberOfEvents()
+    {
+        IEnumerable<XElement> descendants = campEventXML.DescendantsAndSelf("Event");
+        return descendants.Count();
+    }
 }
