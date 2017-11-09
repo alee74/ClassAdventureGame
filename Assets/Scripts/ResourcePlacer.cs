c77dc351302876fd7bcdd3890e62127c58e6fde7
mmaron96 <matthewbm628@gmail.com>
2017-11-09 15:04:37 -0600

    Made a resource placer

---
+++ Assets/Scripts/ResourcePlacer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcePlacer : MonoBehaviour {
+
+
+
+	public GameObject foodPrefab;
+	public GameObject woodPrefab;
+	public GameObject waterPrefab;
+
+	// Use this for initialization
+	void Start () {
+		Vector2 pos = new Vector2(-500, -500);
+		for (int i = -500; i < 500; i++) {
+				for(int j = -500; j < 500; j++){
+					if (Random.Range (0f, 1f) > 0.99f){
+						placeResource (pos);
+					}
+				pos.y = j;
+				}
+			pos.x = i;
+		}
+	}
+
+
+	void placeResource(Vector2 pos){
+		var ran = Random.Range (0f, 1f);
+		if (ran < 0.33f) {
+			Instantiate (foodPrefab, pos, Quaternion.identity);
+		} else if (ran > 0.33f && ran < 0.66f) {
+			Instantiate (woodPrefab, pos, Quaternion.identity);
+		} else {
+			Instantiate (waterPrefab, pos, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
