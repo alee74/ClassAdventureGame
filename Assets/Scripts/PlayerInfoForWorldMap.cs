c37cee502caeed9fed2aaf882e4d5421a3d71db3
daniellekao <dkao@trinity.edu>
2017-11-28 16:09:13 -0600

    started working on getting playa stats to show up on screen - need to pull kaylas code first tho!

--- Assets/Scripts/PlayerInfoForWorldMap.cs
+++ Assets/Scripts/PlayerInfoForWorldMap.cs
@@ -7,15 +7,21 @@ public class PlayerInfoForWorldMap : MonoBehaviour {
     
     private Character currChara;
     private Text nameText;
+    private Text strengthText;
+    private Text staminaText;
     
 	// Use this for initialization
 	void Start () {
         currChara = CharInfo.getCurrentCharacter();
         nameText = GameObject.Find("Name").GetComponent<Text>();
+        strengthText = GameObject.Find("Strength").GetComponent<Text>();
+        staminaText = GameObject.Find("Stamina").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         nameText.text = currChara.name;
+        strengthText.text = "Strength: " + currChara.strength.ToString();
+        staminaText.text = "Stamina: " + currChara.stamina.ToString();
 	}
 }
