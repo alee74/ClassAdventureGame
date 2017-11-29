c156334ec60dfc034b9792b66cd0f1e40392a142
Lauren Truong <ltruong1@trinity.edu>
2017-11-29 12:43:32 -0600

    CutTree works...I think

--- Assets/Scripts/CutWood.cs
+++ Assets/Scripts/CutWood.cs
@@ -5,26 +5,48 @@ using UnityEngine;
 public class CutWood : MonoBehaviour {
 
 	private GameObject player;
-	private GameObject tree;
+	private GameObject tree = null;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Player");
-		tree = GameObject.Find ("Tree");
-		Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
-		Vector3 treePos = new Vector3 (tree.transform.position.x, tree.transform.position.y, tree.transform.position.z);
 	}
-	
+
+	/*
 	// Update is called once per frame
-	void Update () {
+	void OnTriggerStay2D(Collider2D col) {
 		// (player.transform.position - target.transform.position).magnitude
-		if (((player.transform.position - tree.transform.position).magnitude < 2f) && Input.GetButtonDown ("GetWood")) {
+		Debug.Log("wood");
+		touchingTree = false;
+		if (Input.GetButtonDown ("GetWood")) {
+			var treehealth = col.gameObject.GetComponent<TreeHealth> ();
+			if (treehealth != null) {
+				treehealth.DamageTaken (1); 
+			}
+		}
+	}
+	*/
+
+	void Update() {
+		if (tree != null && Input.GetButtonDown ("GetWood")) {
+			Debug.Log ("IM TOUCHING IT");
 			var treehealth = tree.GetComponent<TreeHealth> ();
 			if (treehealth != null) {
 				treehealth.DamageTaken (1); 
 			}
 		}
 	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+	//	touchingTree = true;
+		tree = col.gameObject; 
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+	//	touchingTree = false;
+		tree = null;
+	}
+		
 
 //	void OnCollisionEnter2D(Collision2D col) {
 //		//if player is within a certain distance of tree and presses "GetWood", decrement treehealth
--- Assets/Scripts/TreeHealth.cs
+++ Assets/Scripts/TreeHealth.cs
@@ -6,24 +6,24 @@ public class TreeHealth : MonoBehaviour {
 
 	public int treeHealth = 5; //full health
 	public Transform logs; 
-	private GameObject tree; 
 	public float currentHp;  
 	private GameObject player; 
 
 	// Use this for initialization
 	void Start () {
-		tree = GameObject.Find ("Tree");
 		currentHp = treeHealth; 
 		player = GameObject.Find ("Player");
 	}
 
 	public void DamageTaken (int amount) {
 		currentHp -= amount; 
+		Debug.Log (currentHp);
 		if (currentHp <= 0) {
 			currentHp = 0;
-			Destroy (tree); 
+			//location where logs will be instantiated
 			Vector3 newpos = new Vector3 (player.transform.position.x, player.transform.position.y - 2f, player.transform.position.z);
 			Instantiate (logs, newpos, Quaternion.identity);
+			Destroy (gameObject); 
 		}
 	}
 	
