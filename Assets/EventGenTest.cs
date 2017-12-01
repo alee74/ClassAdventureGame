3ef7336ab4b1974e2b2b09983d50e4d5efd37464
KaylaHood <kaylahood1996@gmail.com>
2017-12-01 14:56:51 -0600

    fixed naming of class

--- Assets/EventGenTest.cs
+++ Assets/EventGenTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour {
+public class EventGenTest : MonoBehaviour {
 
     List<CampEvent> campEvents;
     List<GameObject> eventTiles;
