8026e4411f4c6b66ca026bea27c71a5cd240ff85
sahough <shough@trinity.edu>
2017-12-01 17:28:49 -0600

    added names

---
+++ Assets/Scripts/2DFighter/SceneStart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Character character = CharInfo.getCurrentCharacter();
+		Text player = GameObject.Find("PlayerName").GetComponent<Text>();
+		player.text = character.name;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
