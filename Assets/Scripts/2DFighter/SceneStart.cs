61ae739ccf471c395c8fa333d54abe6bd86c27a0
alee74 <alee5@trinity.edu>
2017-12-01 20:33:03 -0600

    Level enemy type selection
    Each level now starts without an enemy in it, picks one of a random type, spawns it at the location specified by the xStart and yStart parameters specified in the main camera's editor, and sets the enemy name text appropriately

--- Assets/Scripts/2DFighter/SceneStart.cs
+++ Assets/Scripts/2DFighter/SceneStart.cs
@@ -5,12 +5,26 @@ using UnityEngine.UI;
 
 public class SceneStart : MonoBehaviour {
 
+    public float xStart;
+    public float yStart;
+
+    private GameObject enemy;
+    private string[] enemyType = new string[] { "Panda" };
+    static private System.Random rando = new System.Random();
+
 	// Use this for initialization
 	void Start () {
 
 		Character character = CharInfo.getCurrentCharacter();
 		Text player = GameObject.Find("PlayerName").GetComponent<Text>();
 		player.text = character.name;
+
+        string enemyName = enemyType[rando.Next(enemyType.Length)];
+        enemy = Resources.Load<GameObject>("Prefabs/2DFighter/" + enemyName);
+        Instantiate(enemy, new Vector3(xStart, yStart, 0f), Quaternion.identity);
+
+        Text enemyNameText = GameObject.Find("EnemyName").GetComponent<Text>();
+        enemyNameText.text = enemyName;
 		
 	}
 	
