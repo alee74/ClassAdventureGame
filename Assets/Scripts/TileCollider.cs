0db2032fb0473f6d7b293ae13a33707b36c5fca7
Andrew Okin <forkk@forkk.net>
2017-12-05 15:13:18 -0600

    Merge branch 'master' into city-gen

---
+++ Assets/Scripts/TileCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TileCollider : MonoBehaviour {
+
+	private bool _touching = false;
+	public float radius=0.2f;
+	public LayerMask mask;
+
+	public bool touching() {
+		_touching = Physics2D.OverlapCircle (transform.position,radius,mask);
+		return _touching;
+	}
+
+}
