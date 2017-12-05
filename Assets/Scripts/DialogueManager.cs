721b2ae56ded13b378465e88c3e370e59636a33c
austinkim710 <austinkim710@gmail.com>
2017-12-05 06:31:38 -0600

    Dialogue Manager
    Handles dialogue text boxes; close boxes with ENTER
    
    -currently no dialogue setup in camp-

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+	public GameObject dBox;
+	public GameObject dText;
+
+	public bool active;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active && Input.GetKeyDown(KeyCode.End)) 
+			{
+			dBox.SetActive(false);
+			active = false;
+			}
+	}
+}
