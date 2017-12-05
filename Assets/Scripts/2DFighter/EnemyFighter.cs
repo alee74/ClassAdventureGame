0e821c46a77ca49e1757d97dbe91c3a52c8a1f75
ianwitecki <iwitecki@trinity.edu>
2017-12-04 21:21:06 -0600

    panda jump animation and enemy jumping. Not fixed

--- Assets/Scripts/2DFighter/EnemyFighter.cs
+++ Assets/Scripts/2DFighter/EnemyFighter.cs
@@ -118,9 +118,13 @@ public class EnemyFighter : Fighter {
     /// </summary>
     protected override void Walk() {
 
-        if (Grounded())
+        if (opponentTransform.position.y > transform.position.y)
         {
-
+            Debug.Log("Enemy Jump");
+            ChangeState(State.Jump);
+        }
+        else
+        {
             if (transform.position.x > opponentTransform.position.x)
                 rgb.velocity = new Vector2(-speed, 0);  // Vector2.left * speed;
             else
@@ -129,7 +133,6 @@ public class EnemyFighter : Fighter {
             // don't check y, because it will always be true when they are both standing on the ground
             if (Mathf.Abs(transform.position.x - opponentTransform.position.x) <= xAttackDist)
                 ChangeState(State.Punch);
-
         }
 
     }
@@ -143,13 +146,34 @@ public class EnemyFighter : Fighter {
     /// otherwise, we stand.
     /// </summary>
     protected override void Punch() {
-        Debug.Log("Enemy Punch");
+
         StartCoroutine(ControlPunchTiming());
 
        /* if (Mathf.Abs(transform.position.x - opponent.position.x) >= xAttackDist)
             ChangeState(State.Walk);
         else*/
         ChangeState(State.Stand);
+
+    }
+    #endregion
+
+    #region protected override void Jump();
+    /// <summary>
+    /// defines Enemy behavior while in the Jump state.
+    /// sets the velocity to account for horizontal movement while airborne.
+    /// starts coroutine to delay the first check for grounded and then check
+    ///  repeatedly until grounded and then change state to stand.
+    /// </summary>
+    protected override void Jump() {
+
+        if (opponentTransform.position.x < transform.position.x) {
+            rgb.velocity = new Vector2(-speed, rgb.velocity.y);
+        }
+        else {
+            rgb.velocity = new Vector2(speed, rgb.velocity.y);
+        }
+
+        StartCoroutine(DelayGroundedCheck());
 
     }
     #endregion
