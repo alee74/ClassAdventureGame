3feeb7d0df717ef2cc8fab6390a8e2604d240c76
Lauren Truong <ltruong1@trinity.edu>
2017-12-05 18:20:27 -0600

    fixing getting water from lake (adding a max)

---
+++ Assets/Scripts/MaxWater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaxWater : MonoBehaviour {
+
+	public int maxWater = 5; //full amount of water
+	public int currentWater;
+	private GameObject player; 
+
+	// Use this for initialization
+	void Start () {
+		currentWater = maxWater; 
+		player = GameObject.Find ("Player"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void waterTaken (int amount) {
+		currentWater -= amount; 
+		if (currentWater <= 0) {
+			currentWater = 0; 
+		}
+	}
+}
