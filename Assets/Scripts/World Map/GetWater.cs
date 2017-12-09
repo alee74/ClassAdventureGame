856f5bd2f63003e2d8a4ed1ab813d31f9dafc736
Lauren Truong <ltruong1@trinity.edu>
2017-12-09 00:45:16 -0600

    water works...Thanks Danielle

--- Assets/Scripts/World Map/GetWater.cs
+++ Assets/Scripts/World Map/GetWater.cs
@@ -6,42 +6,26 @@ public class GetWater : MonoBehaviour {
 
 	private GameObject player;
 	public GameObject lake = null; 
-//	private GameObject lakeTile = null;
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.Find ("Player"); 
+		player = GameObject.Find ("Player");
 	}
-	
-	// Update is called once per frame
-//	void Update() {
-//		if ((player.transform.position - lake.transform.position).magnitude < 2.5f 
-////			&& (player.transform.position - lake.transform.position).magnitude < 3f
-//			&& Input.GetButtonDown("GetWater")) { /*&&  Input.GetButtonDown ("GetWater")*/ 
-//				Instantiate (waterBottle, player.transform.position, Quaternion.identity);
-//
-//		}
-//	}
-//}
 
-	void OnTriggerEnter2D(Collider2D col) {
-		lake = col.gameObject; 
-		var currentWater = lake.GetComponent<MaxWater> (); 
-		if (col.tag == "Lake" && currentWater != null) {
-			ItemsInInventory.num_water++;
-			currentWater.waterTaken (1);
-			Debug.Log (currentWater);
-			//lakeTile = col.gameObject;
+	void OnCollisionEnter2D(Collision2D col) {
+		lake = col.gameObject;
+
+		if (lake.tag == "Lake") {
+			MaxWater maxWaterScript = lake.GetComponent<MaxWater> ();
+			if (maxWaterScript.currentWater > 0) {
+				//add to inventory then dec
+				ItemsInInventory.num_water++;
+				maxWaterScript.waterTaken (1);
+			}
 		}
 	}
-//
-//	void OnTriggerExit2D(Collider2D col) {
-//		lakeTile = null;
-//	}
 
-
-	void OnTriggerExit2D(Collider2D col) {
-		//	touchingTree = false;
+	void OnCollisionExit2D(Collision2D col) {
 		lake = null;
 	}
 }
