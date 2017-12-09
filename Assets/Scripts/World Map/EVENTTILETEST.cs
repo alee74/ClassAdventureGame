b8253b39d34bc89dd866d3d9937e896d6f21ea6d
daniellekao <dkao@trinity.edu>
2017-12-09 00:48:00 -0600

    Uhhhhhhh made test scene using kayla's code

---
+++ Assets/Scripts/World Map/EVENTTILETEST.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EVENTTILETEST : MonoBehaviour {
+
+    private GameObject eventTile;
+    private EventTile eventTileScript;
+
+	// Use this for initialization
+	void Start () {
+        eventTile = GameObject.Find("EventTile");
+        eventTileScript = eventTile.GetComponent<EventTile>();
+        eventTileScript.tileEvent = EncounterCharacterEvent.GenerateRandom();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
