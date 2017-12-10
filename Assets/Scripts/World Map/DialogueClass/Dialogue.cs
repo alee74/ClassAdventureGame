5cff446012d3dcd1888390a44c79a82941ebb058
daniellekao <dkao@trinity.edu>
2017-12-09 20:22:51 -0600

    dialogue class WIP

---
+++ Assets/Scripts/World Map/DialogueClass/Dialogue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue {
+
+    private string dialogueStr;
+    private Queue<char> dialogueQueue;
+
+    public Dialogue(string _dialogueStr)
+    {
+        dialogueStr = _dialogueStr;
+    }
+
+    private void QueueString(string str)
+    {
+        for (int i = 0; i < dialogueStr.Length; ++i)
+        {
+            dialogueQueue.Enqueue(str[i]);
+        }
+    }
+
+    public void DisplayDialogue(Text textUI)
+    {
+
+    }
+
+}
